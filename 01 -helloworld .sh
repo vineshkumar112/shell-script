0e9bd9b005416acbf23c8110f985729be8c2d94c
vineshkumar112 <kumarmo112728@gmail.com>
2023-08-18 11:31:10 +0530

    give a second value

--- 01 -helloworld .sh
+++ 01 -helloworld .sh
@@ -2,10 +2,8 @@
 
 echo "hello world "
 
-PERSON1= ramesh
-PERSON2= suresh
+PERSON1=ramesh
+PERSON2=suresh
 
 echo "$PERSON1: hello $PERSON2"
 echo "$PERSON2:hello $PERSON1"
-echo "$PERSON1:how are you"
-echp "$PERSON2: fine ! you"
